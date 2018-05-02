c51b320ccc0d02ea7ac891f2ad139593d03f57f4
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2018-05-02 17:30:37 -0300

    change the terrabrasilis-util to use revealing module pattern

--- terrabrasilis-util.js
+++ terrabrasilis-util.js
@@ -1,10 +1,8 @@
-module.exports = {
-
-    Stack: () => {
+    var Stack =  (function(){
         //FIFO = first in, first out
-        this.list = new Array();
+        let list = new Array();
     
-        this.insert = function (obj){
+        let insert = function (obj){
             list.forEach(element => {
                 if(element !== obj) {
                     this.list[this.list.length] = obj;
@@ -12,7 +10,7 @@ module.exports = {
             });            
         }
     
-        this.remove = function (){
+        let remove = function (){
             if(this.list.length > 0){
                 var obj = this.list[this.list.length - 1];
                 this.list.splice(this.list.length - 1,1);
@@ -22,7 +20,7 @@ module.exports = {
             }
         }
     
-        this.getLast = function (){
+        let getLast = function (){
             if(this.list.length > 0){
                 return this.list[this.list.length - 1];
             }else{
@@ -30,16 +28,23 @@ module.exports = {
             }
         } 
         
-        this.getLength = function () {
+        let getLength = function () {
             return this.list.length;
         }
-    },
 
-    Queue: () => {
+        return {
+            insert: insert,
+            remove: remove,
+            getLast: getLast,
+            getLength: getLength
+        }
+    })(Stack || {});
+
+    var Queue = (function(){
         // FILO = first in, last out
         this.list = new Array();
     
-        this.insert = function(obj){
+        let insert = function(obj){
             list.forEach(element => {
                 if(element !== obj) {
                     this.list[this.list.length] = obj;
@@ -47,7 +52,7 @@ module.exports = {
             });                       
         }
     
-        this.remove = function(){
+        let remove = function(){
             if(this.list.length > 0){
                 var obj = this.list[0];
                 this.list.splice(0,1);
@@ -57,7 +62,7 @@ module.exports = {
             }
         }
     
-        this.getFirst = function(){
+        let getFirst = function(){
             if(this.list.length > 0){
                 return this.list[0];
             }else{
@@ -65,8 +70,16 @@ module.exports = {
             }
         }
 
-        this.getLength = function() {
+        let getLength = function() {
             return this.list.length;
         }
-    }
-}
+
+        return {
+            insert: insert,
+            remove: remove,
+            getFirst: getFirst,
+            getLength: getLength
+        }
+    })(Queue || {});
+
+    module.exports = {Stack, Queue};
